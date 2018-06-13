58867bf4aa28a160b0940c113df591288e6b1d16
Alexander Temnov (DevExpress) <temnov.alexander@corp.devexpress.com>
2018-06-13 16:16:49 +0300

    add example content

---
+++ CS/SpecifyTextMarkupAnnotationProperties/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraPdfViewer;
+
+namespace SpecifyTextMarkupAnnotationProperties {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            // Load a document.
+            pdfViewer1.LoadDocument("..\\..\\Demo.pdf");
+
+            // Handle the TextMarkupAnnotationCreating event to specify the text markup annotation properties when the annotation is being created. 
+            pdfViewer1.TextMarkupAnnotationCreating += pdfViewer1_TextMarkupAnnotationCreating;
+        }
+
+        private void pdfViewer1_TextMarkupAnnotationCreating(object sender, PdfTextMarkupAnnotationCreatingEventArgs e) {
+            e.Author = "John Smith";
+            e.Comment = "Note.";
+            e.Color = Color.Gold;
+        }
+    }
+}
