d3eacc72b15c1012cf087ec9ee57ac34b60c3794
DevExpressExampleBot <fakeEmail>
2020-10-21 18:15:25 +0300

    Source auto update [skip ci]

--- VB/SpecifyTextMarkup/Form1.vb
+++ VB/SpecifyTextMarkup/Form1.vb
@@ -1,31 +1,28 @@
-﻿Imports System.Drawing
-Imports System.Windows.Forms
-Imports DevExpress.Pdf
+﻿Imports DevExpress.Pdf
 Imports DevExpress.XtraPdfViewer
 
 Namespace SpecifyTextMarkup
-    Partial Public Class Form1
-        Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+	Partial Public Class Form1
+		Inherits DevExpress.XtraBars.Ribbon.RibbonForm
 
-        Public Sub New()
-            InitializeComponent()
+		Public Sub New()
+			InitializeComponent()
 
-            ' Load a document.
-            pdfViewer1.LoadDocument("..\..\Demo.pdf")
+			' Load a document.
+			pdfViewer1.LoadDocument("..\..\Demo.pdf")
 
-            AddHandler pdfViewer1.AnnotationCreating, AddressOf pdfViewer1_TextMarkupAnnotationCreating
-        End Sub
+			AddHandler pdfViewer1.AnnotationCreating, AddressOf pdfViewer1_TextMarkupAnnotationCreating
+		End Sub
 
-        Private Sub pdfViewer1_TextMarkupAnnotationCreating(ByVal sender As Object, ByVal e As PdfAnnotationCreatingEventArgs) Handles pdfViewer1.AnnotationCreating
-
-            'Specify options for text markup and text annotations:
-            If e.Builder.AnnotationType = PdfAnnotationType.TextMarkup OrElse e.Builder.AnnotationType = PdfAnnotationType.Text Then
-                'Retrieve common options for text and text markup annotations:
-                Dim annotationBuilder = e.Builder.AsMarkupAnnotationBuilder()
-                annotationBuilder.Author = "John Smith"
-                annotationBuilder.Color = New PdfRGBColor(0.2, 0.6, 1.0)
-                annotationBuilder.Contents = "Note"
-            End If
-        End Sub
-    End Class
+		Private Sub pdfViewer1_TextMarkupAnnotationCreating(ByVal sender As Object, ByVal e As PdfAnnotationCreatingEventArgs) Handles pdfViewer1.AnnotationCreating
+			'Specify options for text markup and text annotations:
+			If e.Builder.AnnotationType = PdfAnnotationType.TextMarkup OrElse e.Builder.AnnotationType = PdfAnnotationType.Text Then
+				'Retrieve common options for text and text markup annotations:
+				Dim annotationBuilder = e.Builder.AsMarkupAnnotationBuilder()
+				annotationBuilder.Author = "John Smith"
+				annotationBuilder.Color = New PdfRGBColor(0.20, 0.60, 1.00)
+				annotationBuilder.Contents = "Note"
+			End If
+		End Sub
+	End Class
 End Namespace
